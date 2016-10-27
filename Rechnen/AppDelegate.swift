67aca380f070b9e65f62d424779ffa32fa60453a
René Lieb <rlieb@csc.com>
2016-10-27 08:16:01 +0200

    Initial Commit

---
+++ Rechnen/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Rechnen
+//
+//  Created by René Lieb on 27.10.16.
+//  Copyright © 2016 René Lieb. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Rechnen/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Rechnen
+//
+//  Created by René Lieb on 27.10.16.
+//  Copyright © 2016 René Lieb. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
